b144e7bc918860d2de206f64876cadec4856d879
Sherlock Holo <sherlockya@gmail.com>
2018-06-21 02:30:25 +0800

    Add ack cmd
    Signed-off-by: Sherlock Holo <sherlockya@gmail.com>

--- packet.go
+++ packet.go
@@ -15,12 +15,13 @@ func (e VersionErr) Error() string {
 }
 
 const (
-	Version      = 1
+	Version      = 2
 	HeaderLength = 1 + 4 + 1 + 2
 
 	PSH  = 128
 	FIN  = 64
 	PING = 32
+	ACK  = 16 // 2 bytes data, uint16, the other size has read [uint16] bytes data
 )
 
 type PacketHeader []byte
@@ -47,11 +48,12 @@ type Packet struct {
 	// PSH  0b1000,0000
 	// FIN  0b0100,0000
 	// PING 0b0010,0000
+	// ACK  0b0001,0000
 	// RSV  0b0000,0000
 	CMD uint8
 
-	Length  uint16
-	Payload []byte
+	PayloadLength uint16
+	Payload       []byte
 }
 
 func newPacket(id uint32, status string, payload []byte) *Packet {
@@ -70,13 +72,16 @@ func newPacket(id uint32, status string, payload []byte) *Packet {
 	case "PING":
 		packet.CMD = PING
 
+	case "ACK":
+		packet.CMD = ACK
+
 	default:
 		panic("not allow status " + status)
 	}
 
 	if payload != nil {
 		packet.Payload = payload
-		packet.Length = uint16(len(payload))
+		packet.PayloadLength = uint16(len(payload))
 	}
 
 	return &packet
@@ -102,23 +107,26 @@ func (p *Packet) Bytes() []byte {
 	b[0] = p.Version
 	binary.BigEndian.PutUint32(b[1:], p.ID)
 
-	var status uint8
+	var cmdByte uint8
 
 	switch p.CMD {
 	case PSH:
-		status |= 1 << 7
+		cmdByte |= 1 << 7
 
 	case FIN:
-		status |= 1 << 6
+		cmdByte |= 1 << 6
 
 	case PING:
-		status |= 1 << 5
+		cmdByte |= 1 << 5
+
+	case ACK:
+		cmdByte |= 1 << 4
 	}
 
-	b = append(b, status)
+	b = append(b, cmdByte)
 
 	length := make([]byte, 2)
-	binary.BigEndian.PutUint16(length, p.Length)
+	binary.BigEndian.PutUint16(length, p.PayloadLength)
 	b = append(b, length...)
 
 	b = append(b, p.Payload...)
@@ -137,23 +145,27 @@ func Decode(b []byte) (*Packet, error) {
 
 	p.ID = binary.BigEndian.Uint32(b[1:5])
 
-	status := b[5]
+	cmdByte := b[5]
 
-	if status&(1<<7) != 0 {
+	if cmdByte&(1<<7) != 0 {
 		p.CMD = PSH
 	}
 
-	if status&(1<<6) != 0 {
+	if cmdByte&(1<<6) != 0 {
 		p.CMD = FIN
 	}
 
-	if status&(1<<5) != 0 {
+	if cmdByte&(1<<5) != 0 {
 		p.CMD = PING
 	}
 
-	p.Length = binary.BigEndian.Uint16(b[6:8])
+	if cmdByte&(1<<4) != 0 {
+		p.CMD = ACK
+	}
 
-	if p.Length != 0 {
+	p.PayloadLength = binary.BigEndian.Uint16(b[6:8])
+
+	if p.PayloadLength != 0 {
 		p.Payload = b[8:]
 	}
 
