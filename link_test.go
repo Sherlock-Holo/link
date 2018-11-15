e600b73b94abfd21ccb159d72e23c0a3cf305300
Sherlock Holo <sherlockya@gmail.com>
2018-11-15 20:59:16 +0800

    Refine test
    Signed-off-by: Sherlock Holo <sherlockya@gmail.com>

--- link_test.go
+++ link_test.go
@@ -9,8 +9,8 @@ import (
 	"github.com/akutz/memconn"
 )
 
-func initTest(t *testing.T) (client net.Conn, server net.Conn) {
-	listener, err := memconn.Listen("memb", "test")
+func initTest(t *testing.T, addr string) (client net.Conn, server net.Conn) {
+	listener, err := memconn.Listen("memb", addr)
 	if err != nil {
 		t.Fatal(err)
 	}
@@ -20,7 +20,7 @@ func initTest(t *testing.T) (client net.Conn, server net.Conn) {
 	wg.Add(1)
 	go func() {
 		var err error
-		client, err = memconn.Dial("memb", "test")
+		client, err = memconn.Dial("memb", addr)
 		if err != nil {
 			t.Fatal(err)
 		}
@@ -42,10 +42,15 @@ func initTest(t *testing.T) (client net.Conn, server net.Conn) {
 	return
 }
 
-func TestLink(t *testing.T) {
-	client, server := initTest(t)
+func TestLinkClientToServer(t *testing.T) {
+	client, server := initTest(t, "client to server")
+	defer func() {
+		client.Close()
+		server.Close()
+	}()
 
-	clientManager := NewManager(client, nil)
+	config := DefaultConfig()
+	clientManager := NewManager(client, config)
 	serverManager := NewManager(server, nil)
 
 	fileB, err := ioutil.ReadFile("testdata/more-than-65535")
@@ -70,6 +75,61 @@ func TestLink(t *testing.T) {
 		t.Fatal(err)
 	}
 	defer link.Close()
+
+	b, err := ioutil.ReadAll(link)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	if string(b) != string(fileB) {
+		t.Log("failed")
+	}
+}
+
+func TestLinkServerToClient(t *testing.T) {
+	client, server := initTest(t, "server to client")
+	defer func() {
+		client.Close()
+		server.Close()
+	}()
+
+	config := DefaultConfig()
+	clientManager := NewManager(client, config)
+	serverManager := NewManager(server, nil)
+
+	fileB, err := ioutil.ReadFile("testdata/more-than-65535")
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	go func() {
+		link, err := serverManager.Accept()
+		if err != nil {
+			t.Fatal(err)
+		}
+		defer link.Close()
+
+		// need to run before Write, read the byte which client written
+		if _, err := link.Read(make([]byte, 1)); err != nil {
+			t.Fatal(err)
+		}
+
+		if _, err := link.Write(fileB); err != nil {
+			t.Fatal(err)
+		}
+
+	}()
+
+	link, err := clientManager.Dial()
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer link.Close()
+
+	// need to run before ReadAll, ensure server know client wants to create a link
+	if _, err := link.Write([]byte{1}); err != nil {
+		t.Fatal(err)
+	}
 
 	b, err := ioutil.ReadAll(link)
 	if err != nil {
